8f8b0fb6f49ee5a4c3a8cc04115097fd8b2bf4c0
Gabe Smith <gabriel-smith-hi@github.com>
2020-11-19 16:40:44 -0500

    added User and Page tables

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
-const layout = require('./views/layout.js')
+const { db, Page, User } = require('./models');
+const layout = require('./views/layout.js');
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname + "/public"));
-app.use(express.urlencoded({extended: false}))
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send(layout(''));
-})
+});
+
+db.authenticate().then(() => {
+  console.log('connected to the database');
+});
+
+async function modelsSync() {
+  await db.sync({force: true})
+  // if we want to drop the tables {force: true}
+}
+
+modelsSync()
 
 app.listen(3000, () => {
   console.log('App listening in port 3000');
-})
+});
+
+
