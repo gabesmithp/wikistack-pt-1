bff1b65a742781d667485bb744c5af53015348d9
Kelvin Lin <kelvindlin97@gmail.com>
2020-11-19 15:22:47 -0500

    first part

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const layout = require('./views/layout.js')
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send(layout(''));
+})
+
+app.listen(3000, () => {
+  console.log('App listening in port 3000');
 })
